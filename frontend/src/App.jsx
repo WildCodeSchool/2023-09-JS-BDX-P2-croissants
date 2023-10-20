fb5744f4d2a3d455bf247fafc4a1fab85a519f69
David Duranty <dadaduranty@gmail.com>
2023-10-20 16:07:29 +0200

    display api + mooviecard working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,33 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [api, setApi] = useState();
+  const [api, setApi] = useState([]);
 
   useEffect(() => {
-    axios.get("https://ghibliapi.vercel.app/films")
-    .then((res) => setApi(res))
-  }, [])
+    axios.get("https://ghibliapi.vercel.app/films").then(({ data }) => {
+      setApi(data);
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const { data, headers } = await axios.get(
+  //       "https://ghibliapi.vercel.app/films"
+  //     );
+  //     setApi(data);
+  //   };
+  //   getData();
+  // });
 
   return (
     <div>
       <NavBar />
-      <FilmOfDay/>
-      < MoovieCard api={api}/>
+      <FilmOfDay />
+      {/* {api.map((moovie) => (
+        ))} */}
+      
+        <MoovieCard api={api} key={api.id} />
+      
       <Footer />
     </div>
   );
