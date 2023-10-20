76bb8cb11654713b2804781c4617c75402bed9dc
David Duranty <dadaduranty@gmail.com>
2023-10-20 16:38:46 +0200

    maj css

--- frontend/src/components/MoovieCard.jsx
+++ frontend/src/components/MoovieCard.jsx
@@ -3,7 +3,7 @@ function MoovieCard({ api }) {
     <div className="moovie-container">
       {api.map((moovie) => (
         <div key={moovie.id} className="moovie-div">
-          <img src={moovie.image} alt={moovie.title} />
+          <img className="moovie-img" src={moovie.image} alt={moovie.title} />
           <h3 className="moovie-h3">{moovie.title}</h3>
         </div>
       ))}
