0bae4b89f2c516d18de0dbd65b354453faf4bb4f
David Duranty <dadaduranty@gmail.com>
2023-10-20 10:50:30 +0200

    Add component dailyMoovie

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import FilmOfDay from "./components/FilmOfDay";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <FilmOfDay/>
       <Footer />
     </div>
   );
---
+++ frontend/src/pages/APropos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const APropos = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default APropos;
