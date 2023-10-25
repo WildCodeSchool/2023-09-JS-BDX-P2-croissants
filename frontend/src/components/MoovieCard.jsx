d15b7de36e7e78e67347ab8757aa301ab32fadf6
Victor G <victorgj95@outlook.com>
2023-10-25 17:28:21 +0200

    MoviePage link api + route

--- frontend/src/components/MoovieCard.jsx
+++ frontend/src/components/MoovieCard.jsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
+
 function MoovieCard({ api }) {
   return (
     <div className="moovie-container">
       {api.map((moovie) => (
-        <div key={moovie.id} className="moovie-div">
+        <Link
+          key={moovie.id}
+          className="moovie-div"
+          to={`/thisMoovie/${moovie.title}`}
+        >
           <img className="moovie-img" src={moovie.image} alt={moovie.title} />
           <h3 className="moovie-h3">{moovie.title}</h3>
           <p>{moovie.release_date}</p>
           <div>⭐ : {moovie.rt_score}</div>
-        </div>
+        </Link>
       ))}
     </div>
   );
