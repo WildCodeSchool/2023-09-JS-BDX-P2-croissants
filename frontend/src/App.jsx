6f9701ef3bc5b68d9f4f620ec4d13ef14c2f51da
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2023-10-31 12:00:06 +0100

    [ADD] there's potion flying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Filter from "./components/Filter";
 import FilmOfDay from "./components/FilmOfDay";
 import BackToTopButton from "./components/BackToTopButton";
 import FilterContextProvider from "./context/FilterContext";
+import SkyAnimation from "./components/SkyAnimation";
 
 function App() {
   const [api, setApi] = useState([]); // La liste des films
@@ -48,6 +49,7 @@ function App() {
           <FilmOfDay />
           <Filter />
           <MoovieCard api={api} />
+          <SkyAnimation />
         </div>
         <Footer />
         <BackToTopButton />
---
+++ frontend/src/components/SkyAnimation.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function SkyAnimation() {
+  return (
+    <div className="sky">
+      <img
+        src="src/assets/potion.png"
+        alt="Sprite 1"
+        className="sprite sprite1"
+      />
+      <img
+        src="src/assets/potion.png"
+        alt="Sprite 2"
+        className="sprite sprite2"
+      />
+      <img
+        src="src/assets/potion.png"
+        alt="Sprite 3"
+        className="sprite sprite3"
+      />
+      <img
+        src="src/assets/potion.png"
+        alt="Sprite 4"
+        className="sprite sprite4"
+      />
+      <img
+        src="src/assets/potion.png"
+        alt="Sprite diagonal 1"
+        className="sprite dsprite dsprite1"
+      />
+      <img
+        src="src/assets/potion.png"
+        alt="Sprite diagonal 2"
+        className="sprite dsprite dsprite2"
+      />
+      <img
+        src="src/assets/potion.png"
+        alt="Sprite diagonal 3"
+        className="sprite dsprite dsprite3"
+      />
+      <img
+        src="src/assets/potion.png"
+        alt="Sprite diagonal 4"
+        className="sprite dsprite dsprite4"
+      />
+    </div>
+  );
+}
+
+export default SkyAnimation;
