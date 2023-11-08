1e0682ea0d27407b613c12ba31ce0ec594eff00d
Victor G <victorgj95@outlook.com>
2023-11-08 15:56:22 +0100

    CSS

--- frontend/src/pages/PageFilms.jsx
+++ frontend/src/pages/PageFilms.jsx
@@ -31,26 +31,31 @@ function PageFilm() {
 
   return (
     <main id="pageFilm">
-      <div id="img-film-container">
-        <img
-          id="img-film"
-          src={thisMoovie?.image}
-          alt="img du film, 16rem x 16rem"
-        />
-      </div>
-      <h2 id="movie-title-page">
-        {thisMoovie?.original_title_romanised} <br />
-        {thisMoovie?.original_title}
-      </h2>
-      <div className="synopsis-infos">
-        {thisMoovie?.description}
-        <div id="more-infos">
-          <h4>Director: {thisMoovie?.director}</h4>
-          <h5>Producer: {thisMoovie?.producer}</h5>
-          <h6>Characters:</h6>
-          {characters.map((character) => (
-            <p className="characters">{character}</p>
-          ))}
+      <div className="img-desc">
+        <div id="img-film-container">
+          <img
+            id="img-film"
+            src={thisMoovie?.image}
+            alt="img du film, 16rem x 16rem"
+          />
+        </div>
+
+        <div className="synopsis-infos">
+          <h2 id="movie-title-page">
+            {thisMoovie?.original_title_romanised} <br />
+            {thisMoovie?.original_title}
+          </h2>
+          {thisMoovie?.description}
+          <div id="more-infos">
+            <h4>Director: {thisMoovie?.director}</h4>
+            <h5>Producer: {thisMoovie?.producer}</h5>
+            <h6>
+              Characters:
+              {characters.map((character) => (
+                <p className="characters">{character}</p>
+              ))}
+            </h6>
+          </div>
         </div>
       </div>
       <div id="trailer-container">
