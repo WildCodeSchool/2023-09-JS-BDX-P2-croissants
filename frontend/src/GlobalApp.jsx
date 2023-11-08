bcbbde4fc85393e8c7b4aad4bdeda1c7162953bf
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2023-11-08 15:49:13 +0100

    [FIX] navbar in main

--- frontend/src/GlobalApp.jsx
+++ frontend/src/GlobalApp.jsx
@@ -1,16 +1,21 @@
+import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import { ApiProvider } from "./context/ApiContext";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import FilterContextProvider, { FilterContext } from "./context/FilterContext";
 
 function GlobalApp() {
+  const { searchTerm, setSearchTerm } = useContext(FilterContext); // La recherche
   return (
     <ApiProvider>
-      <NavBar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+      <FilterContextProvider>
+        <NavBar setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </FilterContextProvider>
     </ApiProvider>
   );
 }
