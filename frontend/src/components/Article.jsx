1598ef21da081a45bf27b56cbbb1cad3c1e89e7b
dlavaury <dlavaury@student.42.fr>
2023-11-17 06:07:45 +0100

    Updates

--- frontend/src/components/Article.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Article() {
-  return <h1>Bonjour</h1>;
-}
-
-export default Article;
--- frontend/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>Copyright © Croissants Tm ™ 2023</p>
-    </footer>
-  );
-}
-export default Footer;
